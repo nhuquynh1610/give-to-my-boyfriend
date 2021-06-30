9326d15c7a64250a2c4b98d03d9fa1eeba376fc5
nhuquynh1610 <86746248+nhuquynh1610@users.noreply.github.com>
2021-07-01 04:17:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hello my pig!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    introTitle: 'Hello my pig !',
+    introDesc: `Thank you for being a part of my life`,
+    btnIntro: 'Love u ~',
+    title: 'Anh có yêu em khoonggg?',
+    desc: 'Can you love me more? <3 ',
+    btnYes: 'Anh yêu em',
+    btnNo: 'Không, Bae à :(( ',
+    question:'Lí do tại sao anh yêu em ?',
+    btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Em biết mà. Em cũng yêu anhhh :33',
+    messDesc: 'Yêu em nhiều hơn nhé <3',
+    btnAccept: 'Okiiiii luônnn',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
