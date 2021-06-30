70425b4d10fe3bc786bcbca855d426a4ae831d6c
nhuquynh1610 <86746248+nhuquynh1610@users.noreply.github.com>
2021-07-01 03:56:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Hello my pig !',
+    introTitle: 'Hello my pig!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
